9dda1f08cf03c0336b4c8463eb1fa3f82735966b
HJK <danyil.butov@gmail.com>
2025-03-10 22:19:11 +0000

    refactor: make test changes to make code better

--- gateway/cmd/main.go
+++ gateway/cmd/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"context"
-	"github.com/gin-gonic/gin"
-	"log"
+	"github.com/kelseyhightower/envconfig"
+	"go.uber.org/zap"
 	"net/http"
 	"os"
 	"os/signal"
@@ -13,21 +13,19 @@ import (
 	"github.com/HJyup/translatify-common/discovery"
 	"github.com/HJyup/translatify-common/discovery/consul"
 	"github.com/HJyup/translatify-common/tracer"
-	"github.com/HJyup/translatify-common/utils"
 	"github.com/HJyup/translatify-gateway/internal/gateway/chat"
 	"github.com/HJyup/translatify-gateway/internal/gateway/user"
 	"github.com/HJyup/translatify-gateway/internal/handlers"
 	mux2 "github.com/gorilla/mux"
-	_ "github.com/joho/godotenv/autoload"
 )
 
-var (
-	serviceName = "gateway"
-	httpAddr    = utils.EnvString("GATEWAY_ADDR")
-	consulAddr  = utils.EnvString("CONSUL_ADDR")
-	environment = utils.EnvString("ENVIRONMENT")
-	jaegerAddr  = utils.EnvString("JAEGER_ADDR")
-)
+type Specification struct {
+	ServiceName string `config:"service_name" required:"true" default:"gateway"`
+	Address     string `config:"GATEWAY_ADDR" required:"true"`
+	Consul      string `config:"CONSUL_ADDR" required:"true"`
+	Environment string `config:"ENVIRONMENT" required:"true"`
+	Jaeger      string `config:"JAEGER_ADDR" required:"true"`
+}
 
 // @title           Translatify API
 // @version         1.0
@@ -50,17 +48,21 @@ var (
 
 // @securityDefinitions.basic  BasicAuth
 
-func adaptGinHandler(ginHandler gin.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		// create a gin context from the standard request and response
-		c, _ := gin.CreateTestContext(w)
-		c.Request = r
-		ginHandler(c)
-	}
-}
-
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
+	logger, err := zap.NewProduction()
+	if err != nil {
+		panic("Failed to create logger: " + err.Error())
+	}
+	defer func(logger *zap.Logger) {
+		_ = logger.Sync()
+	}(logger)
+
+	var s Specification
+	if err = envconfig.Process("gateway", &s); err != nil {
+		logger.Fatal("Failed to process environment variables", zap.Error(err))
+	}
+
 	defer cancel()
 
 	sigs := make(chan os.Signal, 1)
@@ -71,46 +73,37 @@ func main() {
 	}()
 
 	tracerCfg := tracer.Config{
-		ServiceName:    serviceName,
+		ServiceName:    s.ServiceName,
 		ServiceVersion: "1.0.0",
-		Environment:    environment,
-		ExporterAddr:   jaegerAddr,
+		Environment:    s.Environment,
+		ExporterAddr:   s.Jaeger,
 		Insecure:       false,
 		Timeout:        5 * time.Second,
 	}
 	tp, err := tracer.InitTracer(ctx, tracerCfg)
 	if err != nil {
-		log.Fatalf("Failed to set global tracer: %v", err)
+		logger.Fatal("Failed to set global tracer", zap.Error(err))
 	}
 	defer func() {
 		if err = tracer.ShutdownTracer(ctx, tp); err != nil {
-			log.Printf("Failed to shutdown tracer: %v", err)
+			logger.Error("Failed to shutdown tracer", zap.Error(err))
 		}
 	}()
 
-	registry, err := consul.NewRegistry(consulAddr)
+	registry, err := consul.NewRegistry(s.Consul)
 	if err != nil {
-		log.Fatalf("Failed to create registry: %v", err)
+		logger.Fatal("Failed to create registry", zap.Error(err))
 	}
 
-	instanceID := discovery.GenerateInstanceID(serviceName)
-	if err = registry.Register(instanceID, serviceName, httpAddr); err != nil {
-		log.Fatalf("Failed to register service: %v", err)
+	instanceID := discovery.GenerateInstanceID(s.ServiceName)
+	if err = registry.Register(instanceID, s.ServiceName, s.Address); err != nil {
+		logger.Fatal("Failed to register service", zap.Error(err))
 	}
-	go func() {
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			default:
-				if err = registry.HealthCheck(instanceID); err != nil {
-					log.Printf("Failed to health check: %v", err)
-				}
-				time.Sleep(1 * time.Second)
-			}
+	defer func() {
+		if err = registry.DeRegister(instanceID); err != nil {
+			logger.Error("Failed to deregister service", zap.Error(err))
 		}
 	}()
-	defer registry.DeRegister(instanceID)
 
 	router := mux2.NewRouter()
 
@@ -125,8 +118,8 @@ func main() {
 	refHandler := handlers.NewReferenceHandler()
 	refHandler.RegisterRoutes(router)
 
-	log.Println("Starting server on", httpAddr)
-	if err = http.ListenAndServe(httpAddr, router); err != nil {
-		log.Fatalf("Failed to start server: %v", err)
+	logger.Info("Starting server", zap.String("address", s.Address))
+	if err = http.ListenAndServe(s.Address, router); err != nil {
+		logger.Fatal("Failed to start server", zap.Error(err))
 	}
 }
--- gateway/go.mod
+++ gateway/go.mod
@@ -2,31 +2,20 @@ module github.com/HJyup/translatify-gateway
 
 go 1.22.0
 
-toolchain go1.23.4
-
 require (
 	github.com/HJyup/translatify-common v0.0.0
 	github.com/MarceloPetrucio/go-scalar-api-reference v0.0.0-20240521013641-ce5d2efe0e06
-	github.com/gin-gonic/gin v1.9.0
 	github.com/gorilla/mux v1.8.1
 	github.com/gorilla/websocket v1.5.3
-	github.com/joho/godotenv v1.5.1
 	go.opentelemetry.io/otel v1.34.0
 )
 
 require (
 	github.com/armon/go-metrics v0.4.1 // indirect
-	github.com/bytedance/sonic v1.8.0 // indirect
 	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
-	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/fatih/color v1.16.0 // indirect
-	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
-	github.com/go-playground/locales v0.14.1 // indirect
-	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.11.2 // indirect
-	github.com/goccy/go-json v0.10.0 // indirect
 	github.com/golang-jwt/jwt/v4 v4.5.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.25.1 // indirect
@@ -39,18 +28,11 @@ require (
 	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
 	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	github.com/hashicorp/serf v0.10.1 // indirect
-	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
-	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/kelseyhightower/envconfig v1.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
-	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
-	github.com/ugorji/go/codec v1.2.9 // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.59.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.34.0 // indirect
@@ -59,8 +41,8 @@ require (
 	go.opentelemetry.io/otel/sdk v1.34.0 // indirect
 	go.opentelemetry.io/otel/trace v1.34.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.5.0 // indirect
-	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
-	golang.org/x/crypto v0.33.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/exp v0.0.0-20230817173708-d852ddb80c63 // indirect
 	golang.org/x/net v0.35.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
